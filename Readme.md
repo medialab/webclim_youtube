c41ddee8f60700d4644e68820870532ad3299010
Danaal <danaalmanla@hotmail.com>
2021-07-21 14:46:50 +0200

    add project files

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# YouTube Recommendation data collection
+This reporistory contains two approahes to collect data from youtube with web scraping. The aim of these experiments is to 
+simulate the user behaviour.
+### 1- Experiment 1: Collect the recommendation based on search word(s)
+Run the following command
+```
+  python3 code/experiment_2.py "['search_word_1','search_word_2']"
+```
+After running the code csv files will be created for every search word in the data file. The collection is done for the recommendations in the main search page. Then for every video in the main list the system will go through each and collect
+the recommended/related videos. 
+### 1- Experiment 2: Collect the recommendation based on starting with a video_id
+Run the following command
+```
+  python3 code/experiment_2.py "['video_id_1','video_id_2']"
+```
+After running the code csv files will be created for every video in the list and it will be in the data file. The collection is done
+for the recommended/related videos for the starting video then the system will go through the top 10 videos and collect their recommendations.
+
+
+To simulate user behaviour the system play part of the videos either half of it or around 1 min based on the length of the video. You can change
+these numbers in the ```time.sleep()``` functions.
---
+++ code/experiment_2.py
@@ -0,0 +1,192 @@
+import uuid
+import time
+import re
+import os
+import sys
+import ast
+
+import numpy as np
+import pandas as pd
+from googleapiclient.discovery import build
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from dotenv import load_dotenv
+
+
+
+
+def get_data_video(v_url,curr_depth, vid_id,parent_id = 'None',level='None',order='None'):
+        data = np.array([])
+        code =  v_url.split('v=')[1][0:11]
+        vid_suff = code
+        request = youtube.videos().list(
+        part="snippet,contentDetails,statistics",
+        id=vid_suff
+        )
+        response = request.execute()
+        try:
+
+            main_info = response['items'][0]['snippet']
+
+            channel_name = main_info['channelTitle']
+            video_title= main_info['title']
+            channel_ids = main_info['channelId']
+            published_ats = main_info['publishedAt']
+            video_ids = response['items'][0]['id']
+            view_counts = response['items'][0]['statistics']['viewCount']
+            try:
+                likes = response['items'][0]['statistics']['likeCount']
+            except:
+                likes = 'non'
+            try:
+                dislikes = response['items'][0]['statistics']['dislikeCount']
+            except:
+                dislikes = 'non'
+            try:
+                comments = response['items'][0]['statistics']['commentCount']
+            except:
+                comments = 'non'
+            duration = re.findall(r'\d+', response['items'][0]['contentDetails']['duration'])
+            time_to_run = time_collect(duration)
+            #time_to_run = 5
+            request_channel = youtube.channels().list(
+            part="statistics",
+            id= main_info['channelId']
+        )
+            data = np.append(data,np.array([vid_id,video_title,view_counts,likes,dislikes,comments , video_ids,channel_name,channel_ids,published_ats,duration]))
+            channels_info = request_channel.execute()
+            if (channels_info['items'][0]['statistics']['hiddenSubscriberCount']==False):
+
+                subs_count = channels_info['items'][0]['statistics']['subscriberCount']
+            else:
+                subs_count = None
+            data = np.append(data,[subs_count,v_url,level,parent_id,order])
+        except:
+            print('Error')
+        return data,time_to_run
+
+def time_collect(duration):
+    if (len(duration)>2):
+            return 5*60
+    elif(len(duration)==2):
+        if (int(duration[1])<10):
+            return int(duration[1])*60
+        else:
+            return 5*60
+    elif(len(duration)==1):
+        return int(duration[0])
+
+
+def random_walk(depth, vid_to_start,youtube):
+    # depth 1 --> look for first level of recommendation
+    # depth 2 --> click on a recommendation then move to the next video with its recommendation
+    data = pd.DataFrame([], columns=['id', 'video_title', 'view_counts', 'likes', 'dislikes', 'comments', 'video_id',
+                                     'channel_name', 'channel_id', 'published_at', 'duration', 'subscriber_count',
+                                     'video_url', 'level', 'parent_id', 'order'])
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    baseurl = "http://youtube.com"
+    driver.get("http://youtube.com")
+    driver.get(f'{baseurl}/watch?v={vid_to_start}')
+    vid = f'{baseurl}/watch?v={vid_to_start}'
+    list_youtube = []
+
+    vid_id = uuid.uuid4()
+    time.sleep(10)
+    try:
+        button = driver.find_element_by_class_name('ytp-ad-skip-button-container')
+        button.click()
+    except:
+        print('no ad')
+
+    # time.sleep(30)
+    curr = 0
+    x_data, duration = get_data_video(vid, curr, vid_id)
+    time.sleep(duration)
+    a_series = pd.Series(x_data, index=data.columns)
+    data = data.append(a_series, ignore_index=True)
+
+    depth_current = 0
+    wait = WebDriverWait(driver, 10)
+    presence = EC.presence_of_element_located
+    wait.until(presence((By.ID, "related")))
+    list_recommendation = driver.find_elements_by_xpath(('//*[@id="dismissible"]/div/div[1]/a'))
+    # print(list_recommendation)
+    recos = []
+    for i in range(0, len(list_recommendation)):
+        recos.append(list_recommendation[i].get_attribute("href"))
+    for reco in range(0, 10):
+        reco_id = uuid.uuid4()
+        # reco_data,duration = get_data_video(list_recommendation[reco].get_attribute("href"),depth_current,reco_id, vid_id)
+        wait = WebDriverWait(driver, 10)
+        presence = EC.presence_of_element_located
+        wait.until(presence((By.ID, "related")))
+        data_reco = mini_walk(recos[reco], reco_id, vid_id, driver, data, reco)
+        # print(data_reco.shape[1])
+        # print(data.shape[1])
+        # r_series = pd.Series(data_reco, index = data.columns)
+
+        data = data.append(data_reco, ignore_index=True)
+    for reco_s in range(10, len(recos)):
+        reco_data, duration = get_data_video(recos[reco_s], depth_current, reco_id, vid_id, 1, reco_s)
+        r_series = pd.Series(reco_data, index=data.columns)
+
+        data = data.append(r_series, ignore_index=True)
+
+    driver.close()
+    path = 'data/' + vid_to_start + 'experiment_2.csv'
+    data.to_csv(path, index=False)
+
+
+def mini_walk(vid_url, reco_id, main_id, driver, data_s, order):
+    data = pd.DataFrame([], columns=['id', 'video_title', 'view_counts', 'likes', 'dislikes', 'comments', 'video_id',
+                                     'channel_name', 'channel_id', 'published_at', 'duration', 'subscriber_count',
+                                     'video_url', 'level', 'parent_id', 'order'])
+    code = vid_url.split('v=')[1]
+    baseurl = "http://youtube.com"
+    driver.get(f'{baseurl}/watch?v={code}')
+    time.sleep(6)
+    try:
+        button = driver.find_element_by_class_name('ytp-ad-skip-button-container')
+        button.click()
+    except:
+        print('no ad')
+
+    vid_id = uuid.uuid4()
+    curr = 0
+    x_data, duration = get_data_video(f'{baseurl}/watch?v={code}', curr, vid_id, main_id, 1, order)
+    time.sleep(duration)
+    a_series = pd.Series(x_data, index=data_s.columns)
+    data = data.append(a_series, ignore_index=True)
+    depth_current = 0
+    wait = WebDriverWait(driver, 10)
+    presence = EC.presence_of_element_located
+    wait.until(presence((By.ID, "related")))
+    list_recommendation = driver.find_elements_by_xpath(('//*[@id="dismissible"]/div/div[1]/a'))
+    for reco in range(0, len(list_recommendation)):
+        wait = WebDriverWait(driver, 10)
+
+        wait.until(presence((By.ID, "related")))
+        reco_id_t = uuid.uuid4()
+        reco_data, duration = get_data_video(list_recommendation[reco].get_attribute("href"), depth_current, reco_id_t,
+                                             reco_id, 2, reco)
+        r_series = pd.Series(reco_data, index=data_s.columns)
+
+        data = data.append(r_series, ignore_index=True)
+
+    return data
+
+if __name__=="__main__":
+    load_dotenv()
+    # the list_is correspond to the id of channels [OANN, Tony Heller]
+    api_key = os.getenv('YOUTUBE_TOKEN')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    videos = ast.literal_eval(sys.argv[1])
+    for video in videos:
+        random_walk(1, video,youtube)
